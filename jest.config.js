aefeb886d8ccd31e279c97592215490902ddec60
Luismkm <luismarcelo13@yahoo.com.br>
2022-05-02 21:07:32 -0300

    chore: config jest

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,3 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
 module.exports = {
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: "coverage",
