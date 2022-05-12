4d81377271c21a1d11590c55ad7981c5a3d49456
Luismkm <luismarcelo13@yahoo.com.br>
2022-05-12 14:47:34 -0300

    feat: PgUserAccountRepository load method implementation

---
+++ tests/infra/postgres/mocks/connection.ts
@@ -0,0 +1,12 @@
+import { newDb, IMemoryDb, IBackup } from 'pg-mem';
+
+export const makeFakeDb = async (entities?: any[]): Promise<IMemoryDb> => {
+  const db = newDb();
+  const connection = await db.adapters.createTypeormConnection({
+    type: 'postgres',
+    entities: entities ?? ['src/infra/postgres/entities/index.ts'],
+  });
+
+  await connection.synchronize();
+  return db;
+};
