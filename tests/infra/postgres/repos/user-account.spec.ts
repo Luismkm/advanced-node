93fd367f6b58ca15b30340d32c612d3b56da6b0b
Luismkm <luismarcelo13@yahoo.com.br>
2022-05-12 17:09:44 -0300

    test: ensure PgUserAccountRepository updates account if id is defined

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -50,4 +50,26 @@ describe('PgUserAccountRepository', () => {
       expect(pgUser?.id).toEqual(1);
     });
   });
+
+  it('should update an account if id is undefined', async () => {
+    await pgUserRepo.save({
+      email: 'any_email',
+      name: 'any_name',
+      facebookId: 'any_fb_id',
+    });
+    await sut.saveWithFacebook({
+      id: '1',
+      email: 'new_email',
+      name: 'new_name',
+      facebookId: 'new_fb_id',
+    });
+    const pgUser = await pgUserRepo.findOne({ id: 1 });
+
+    expect(pgUser).toEqual({
+      id: 1,
+      email: 'any_email',
+      name: 'new_name',
+      facebookId: 'new_fb_id',
+    });
+  });
 });
