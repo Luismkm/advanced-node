21dc976987e2e3094701665b4e036dfbf69012e0
Luismkm <luismarcelo13@yahoo.com.br>
2022-05-12 14:47:46 -0300

    test: PgUserAccountRepository load method implementation

---
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -0,0 +1,40 @@
+import { IBackup } from 'pg-mem';
+import { getRepository, getConnection, Repository } from 'typeorm';
+
+import { PgUserAccountRepository } from '@/infra/postgres/repos';
+import { PgUser } from '@/infra/postgres/entities';
+import { makeFakeDb } from '@/tests/infra/postgres/mocks';
+
+describe('PgUserAccountRepository', () => {
+  describe('load', () => {
+    let sut: PgUserAccountRepository;
+    let pgUserRepo: Repository<PgUser>;
+    let backup: IBackup;
+
+    beforeAll(async () => {
+      const db = await makeFakeDb([PgUser]);
+      backup = db.backup();
+      pgUserRepo = getRepository(PgUser);
+    });
+
+    afterAll(async () => {
+      await getConnection().close();
+    });
+
+    beforeEach(() => {
+      backup.restore();
+      sut = new PgUserAccountRepository();
+    });
+
+    it('should return an account if email exists', async () => {
+      await pgUserRepo.save({ email: 'any_email' });
+      const account = await sut.load({ email: 'any_email' });
+      expect(account).toEqual({ id: '1' });
+    });
+
+    it('should return undefined if email does not exists', async () => {
+      const account = await sut.load({ email: 'any_email' });
+      expect(account).toBeUndefined();
+    });
+  });
+});
