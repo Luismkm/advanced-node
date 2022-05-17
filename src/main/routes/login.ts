fdff966d301a7c485952081e44337bb9b89a5a10
Luismkm <luismarcelo13@yahoo.com.br>
2022-05-17 19:24:12 -0300

    refactor: add route prefix

--- src/main/routes/login.ts
+++ src/main/routes/login.ts
@@ -4,5 +4,5 @@ import { ExpressRouter } from '@/infra/http';
 
 export default (router: Router): void => {
   const adapter = new ExpressRouter(makeFacebookLoginController());
-  router.post('./api/login/facebook', adapter.adapt);
+  router.post('/login/facebook', adapter.adapt);
 };
