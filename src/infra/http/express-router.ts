b17a6b99d13655601dc7f25b8cf8973a985238a9
Luismkm <luismarcelo13@yahoo.com.br>
2022-05-17 19:09:56 -0300

    refactor: improve code quality

--- src/infra/http/express-router.ts
+++ src/infra/http/express-router.ts
@@ -6,11 +6,8 @@ export class ExpressRouter {
   constructor(private readonly controller: Controller) {}
 
   async adapt(req: Request, res: Response): Promise<void> {
-    const httpResponse = await this.controller.handle({ ...req.body });
-    if (httpResponse.statusCode === 200) {
-      res.status(200).json(httpResponse.data);
-    } else {
-      res.status(httpResponse.statusCode).json({ error: httpResponse.data.message });
-    }
+    const { statusCode, data } = await this.controller.handle({ ...req.body });
+    const json = statusCode === 200 ? data : { error: data.message };
+    res.status(statusCode).json(json);
   }
 }
