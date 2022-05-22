a1b7b8fd5d470b8b1a749278d5f6b8772df4e86d
Luismkm <luismarcelo13@yahoo.com.br>
2022-05-22 14:55:20 -0300

    test: ensure auth middleware returns 200 if authorization header is valid

--- tests/main/middlewares/authentication.spec.ts
+++ tests/main/middlewares/authentication.spec.ts
@@ -1,16 +1,28 @@
 import request from 'supertest';
+import { sign } from 'jsonwebtoken';
 
 import { app } from '@/main/config/app';
-import { ForbiddenError } from '@/application/errors';
 import { auth } from '@/main/middlewares';
+import { env } from '@/main/config/env';
+import { ForbiddenError } from '@/application/errors';
 
 describe('Authentication Middleware', () => {
   it('should return 403 if authorization header was not provided', async () => {
-    app.get('/fake_route', auth, (req, res) => {
-      res.json(req.locals);
-    });
+    app.get('/fake_route', auth);
     const { status, body } = await request(app).get('/fake_route');
     expect(status).toBe(403);
     expect(body.error).toBe(new ForbiddenError().message);
+  });
+
+  it('should return 200 if authorization header was valid', async () => {
+    const authorization = sign({ key: 'any_user_id' }, env.jwtSecret);
+    app.get('/fake_route', auth, (req, res) => {
+      res.json(req.locals);
+    });
+    const { status, body } = await request(app)
+      .get('/fake_route')
+      .set({ authorization });
+    expect(status).toBe(200);
+    expect(body).toEqual({ userId: 'any_user_id' });
   });
 });
