c4274ffef1608bc7c5777761e13013525f2ff0f2
Luismkm <luismarcelo13@yahoo.com.br>
2022-05-23 13:26:25 -0300

    feat: ensure ChangeProfilePicture calls LoadUserProfile with correct input

--- src/domain/contracts/repos/user-profile.ts
+++ src/domain/contracts/repos/user-profile.ts
@@ -1,7 +1,13 @@
 export interface SaveUserPicture {
   savePicture: (input: SaveUserPicture.Input) => Promise<void>
 }
-
 export namespace SaveUserPicture {
   export type Input = { pictureUrl?: string };
+}
+
+export interface LoadUserProfile {
+  load: (params: LoadUserProfile.Input) => Promise<void>
+}
+export namespace LoadUserProfile {
+  export type Input = { id: string };
 }
