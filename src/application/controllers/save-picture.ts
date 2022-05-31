9eb2b503501ee2caa034dec263e1ceef8ae52b99
Luismkm <luismarcelo13@yahoo.com.br>
2022-05-30 21:27:45 -0300

    feat: add required validator

--- src/application/controllers/save-picture.ts
+++ src/application/controllers/save-picture.ts
@@ -3,7 +3,7 @@ import { badRequest, HttpResponse, ok } from '@/application/helpers';
 import { ChangeProfilePicture } from '@/domain/use-cases';
 import { Controller } from '@/application/controllers';
 
-type Httprequest = { file: { buffer: Buffer, mimeType: string }, userId: string };
+type HttpRequest = { file: { buffer: Buffer, mimeType: string }, userId: string };
 type Model = Error | { initials?: string, pictureUrl?: string };
 
 export class SavePictureController extends Controller {
@@ -11,7 +11,7 @@ export class SavePictureController extends Controller {
     super();
   }
 
-  async perform({ file, userId }: Httprequest): Promise<HttpResponse<Model>> {
+  async perform({ file, userId }: HttpRequest): Promise<HttpResponse<Model>> {
     if (file === undefined || file === null) return badRequest(new RequiredFieldError('file'));
     if (file.buffer.length === 0) return badRequest(new RequiredFieldError('file'));
     if (!['image/png', 'image/jpg', 'image/jpeg'].includes(file.mimeType)) return badRequest(new InvalidMimeTypeError(['png', 'jpeg']));
---
+++ src/application/validation/required.ts
@@ -0,0 +1,29 @@
+import { RequiredFieldError } from '@/application/errors';
+
+export class Required {
+  constructor(
+    readonly value: any,
+    readonly fieldName?: string,
+  ) {}
+
+  validate(): Error | undefined {
+    if (this.value === null || this.value === undefined) {
+      return new RequiredFieldError(this.fieldName);
+    }
+  }
+}
+
+export class RequiredString extends Required {
+  constructor(
+    readonly value: string,
+    readonly fieldName?: string,
+  ) {
+    super(value, fieldName);
+  }
+
+  validate(): Error | undefined {
+    if (super.validate() !== undefined || this.value === '') {
+      return new RequiredFieldError(this.fieldName);
+    }
+  }
+}
