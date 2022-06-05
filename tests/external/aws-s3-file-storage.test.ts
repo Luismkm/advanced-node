3b417d2cf87a9f550ef420d831042eabc6e2fc2f
Luismkm <luismarcelo13@yahoo.com.br>
2022-06-05 10:00:39 -0300

    test: add aws s3 integration test

---
+++ tests/external/aws-s3-file-storage.test.ts
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { env } from '@/main/config/env';
+import { AwsS3FileStorage } from '@/infra/gateways';
+
+describe('Aws S3 Integration Tests', () => {
+  let sut: AwsS3FileStorage;
+
+  beforeEach(() => {
+    sut = new AwsS3FileStorage(
+      env.s3.accessKey,
+      env.s3.secret,
+      env.s3.bucket,
+    );
+  });
+  it('should upload and delete image from aws s3', async () => {
+    const onePixelImage = 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQIW2P4z8DwHwAFAAH/F1FwBgAAAABJRU5ErkJggg==';
+    const file = Buffer.from(onePixelImage, 'base64');
+    const key = 'any_key.png';
+    const pictureUrl = await sut.upload({ key, file });
+    expect((await axios.get(pictureUrl)).status).toBe(200);
+
+    await sut.delete({ key });
+    await expect(axios.get(pictureUrl)).rejects.toThrow();
+  });
+});
